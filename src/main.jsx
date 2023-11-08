9bf854ebd269c22469cc31d84d2680c18ec8d5a4
Sergey4857 <yasak.sergey@gmail.com>
2023-11-08 15:41:51 +0200

    depend.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
+import { Global, ThemeProvider } from "@emotion/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { GlobalStyles, theme } from "./styles";
+
+import App from "components/App";
+import Loader from "components/Loader/Loader";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyles} />
+      <Provider store={store}>
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
