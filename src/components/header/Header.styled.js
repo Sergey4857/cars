443e6a84efe6f5dd4ca2477b396447350e43f964
Sergey4857 <yasak.sergey@gmail.com>
2023-11-11 12:55:15 +0200

    header

--- src/components/header/Header.styled.js
+++ src/components/header/Header.styled.js
@@ -1,3 +1,11 @@
 import styled from "@emotion/styled";
 
-export const HeaderStyled = styled.header``;
+export const HeaderStyled = styled.header`
+  display: flex;
+  background-color: rgba(18, 20, 23, 0.5);
+
+  gap: 20px;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+`;
