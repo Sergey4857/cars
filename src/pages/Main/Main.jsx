027e9c92164899bf78d29d69602c2d041cd54a61
Sergey4857 <yasak.sergey@gmail.com>
2023-11-11 12:37:21 +0200

    +

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,11 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../../components/Header/Header";
+import { Container } from "../../components/common/container/Container";
 
 export function Main() {
   return (
     <>
-      <Header />
+      <Container />
       <Suspense>
         <Outlet />
       </Suspense>
