d7165db44e571e5137231d35db31f1a3dd20e419
Sergey4857 <yasak.sergey@gmail.com>
2023-11-11 11:13:36 +0200

    depl

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,12 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Wrap } from "./Header.styled";
 
 export default function Header() {
   return (
-    <>
-      <Wrap>
-        {/* <Link to="/">HomePage</Link> */}
-        <Link to="catalog">catalog</Link>
-        <Link to="favorites">favorites</Link>
-      </Wrap>
-    </>
+    <Wrap>
+      {/* <Link to="/">HomePage</Link>
+      <Link to="catalog">catalog</Link>
+      <Link to="favorites">favorites</Link> */}
+    </Wrap>
   );
 }
