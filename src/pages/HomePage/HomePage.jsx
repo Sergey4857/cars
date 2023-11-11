287689c0b1ed0fe08b5a398c309031265d1a145c
Sergey4857 <yasak.sergey@gmail.com>
2023-11-11 12:29:30 +0200

    +

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,8 @@
-import axios from "axios";
+import { Header } from "../../components/header/Header";
 export function HomePage() {
-  const rentalConditions =
-    "Minimum age: 25\nValid driver's license\nSecurity deposit required";
-  console.log(rentalConditions.split("\n"));
-
-  const BaseUrl = "https://654b817e5b38a59f28ef32f6.mockapi.io/";
-
   return (
     <>
+      <Header />
       <p>Its homepage</p>
     </>
   );
